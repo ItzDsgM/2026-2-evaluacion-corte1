0113c0fa283f1460f3368ae342af2fe270bcc575
emanuelmur2006@gmail.com <emanuelmur2006@gmail.com>
2026-09-08 18:03:18 -0500

    revisar los otros repos para implementar

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -56,7 +56,12 @@ class StudentController{
 
     // TODO (Reto 1 - Bulk create): validar que request.body sea un arreglo y delegar en studentService.bulkCreate
     async bulkCreate(request: Request, response: Response){
-        response.status(501).json({ message: "Not implemented" });
+        try {
+            const students : string[] = request.body as Partial<BulkToggleInput>;
+            if 
+        } catch (error) {
+            response.status(501).json({ message: "Not implemented" });
+        }
     }
 
     // TODO (Reto 2 - Search): tomar los query params y delegar en studentService.search
